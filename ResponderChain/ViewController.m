11798f6c1021399414111acbe68f1056fd2fecec
李响 <601148234@qq.com>
2018-06-28 20:16:50 +0800

    change in dev

--- ResponderChain/ViewController.m
+++ ResponderChain/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    //dev的修改
 }
 
 - (IBAction)btnAction:(UIButton *)sender {
